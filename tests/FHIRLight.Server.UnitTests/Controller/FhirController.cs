b795121d5ce38521f082989cf0f00da8d2ce388d
Verzada <harald.s.hanssen@gmail.com>
2017-02-05 16:09:32 +0100

    Added some canary tests for patientservice

---
+++ tests/FHIRLight.Server.UnitTests/Controller/FhirController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using FHIRLight.Library.Interface;
+using FHIRLight.Services.Service;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace FHIRLight.Server.UnitTests.Controller
+{
+    [TestFixture]
+   public class FhirController
+    {
+        private IFhirLightService _patientService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _patientService = new PatientService();
+        }
+
+        [Test]
+        public void GetMetaData_CanaryTest_True()
+        {
+            var metaData = _patientService.CreateMetaData();
+
+            Assert.IsTrue(metaData != null, "Should not be empty");
+        }
+
+        [Test]
+        public void GetPatientResult_True()
+        {
+            var result = _patientService.Read("1");
+
+            Assert.IsTrue(result != null, "Result should not be null");
+            Assert.IsTrue(result.TypeName == nameof(Patient), "Expected a " + nameof(Patient) + " resource");
+        }
+
+        [Test]
+        public void GetPatient_OperationOutcome_Exception()
+        {
+            var searchParams = new SearchParams();
+            searchParams.Add("Given", "Name");
+
+            //Act
+            ActualValueDelegate<object> testDelegate = () =>_patientService.Read(searchParams);
+
+            //Assert
+            Assert.That(testDelegate, Throws.TypeOf<ArgumentException>());
+
+        }
+       
+    }
+}
