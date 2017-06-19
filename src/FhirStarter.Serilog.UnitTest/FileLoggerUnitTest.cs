de30b853996a4c5d121a5a7e06133a8c07daee71
verzada <harald.s.hanssen@gmail.com>
2017-06-19 16:05:09 +0200

    Updated references and added references to more of serilogs libraries

--- src/FhirStarter.Serilog.UnitTest/FileLoggerUnitTest.cs
+++ src/FhirStarter.Serilog.UnitTest/FileLoggerUnitTest.cs
@@ -3,6 +3,9 @@ using Serilog.Core;
 
 namespace FhirStarter.Serilog.UnitTest
 {
+    // Test with curl:
+    // curl -k -u "x:<token>" http://<hostname>:8088/services/collector/event -d "{\"event\": \"basic auth ftw!\"}"
+
     [TestFixture]
     [Category("FileLogging")]
     public class FileLoggerUnitTest
@@ -24,7 +27,7 @@ namespace FhirStarter.Serilog.UnitTest
         [Test]
         public void WriteToFile_Valid()
         {
-            _logger.Information("This is a test to write to file");
+            _logger.Error("This is a test to write to file");
         }
     }
 }
